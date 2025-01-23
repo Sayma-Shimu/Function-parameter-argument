548e0e25db2345ed1f0e6649e420bb8747c78005
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-01-23 11:38:17 +0600

    parameter & argument

---
+++ index.js
@@ -0,0 +1,79 @@
+/*---------
+
+function meal(){
+    console.log('Alu vorta');
+    console.log('Dim vazi');
+    console.log('Vaat');
+    console.log('--------------------');
+
+}
+
+meal()   */
+
+
+/*-----function multiply-------parameter & argument----------*/
+/*--note:  argument holo function k jeta dibo ........parameter holo variable a ja recieve korbe seta-----.*/ 
+
+/*function multiply(result){
+console.log(result);
+
+}
+
+multiply(5);
+multiply(15);
+multiply(25);   */
+
+
+
+/*function multiply(result){
+let finalResult = result * result;
+console.log(finalResult);
+
+}
+
+multiply(5);
+multiply(15);
+multiply(25);  */
+
+
+/*function calculation(number1, number2){
+const sum = number1 + number2;
+
+console.log(sum);
+
+}
+
+calculation(2,5);
+calculation(864462,989655645);*/
+
+
+/*function math(num1, num2, num3){
+    const multiply = num1 * num2 * num3 
+
+    console.log(multiply);
+
+
+}
+
+math(4,5,6);  */
+
+
+/*function variable(nmbr1, nmbr2, nmbr3, nmbr4, nmbr5){
+const multiply = nmbr1 * nmbr2 * nmbr3 * nmbr4 * nmbr5
+
+console.log(multiply);
+}
+
+
+variable(50, 30, 80, 90, 85);   */
+
+
+/*function sum(a,b,c,d,e,f,g,h){
+const result = a+b+c+d+e+f+g+h
+
+console.log(result);
+}
+
+sum(1,2,3,4,5,6,7,8);  */
+
+
